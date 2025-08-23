379c81a48dc347bd7df0026a930a60f26f09fff2
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-08-23 17:16:53 +0330

    Feat:add heder

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ const mobileMenu = document.getElementById("mobileMenu");
 hamburger.addEventListener("click", () => {
   if (mobileMenu.style.display === "flex") {
     mobileMenu.style.display = "none";
-    hamburger.innerHTML = "☰"; // menu icon
+    hamburger.innerHTML = "☰";
   } else {
     mobileMenu.style.display = "flex";
-    hamburger.innerHTML = "✖"; // close icon
+    hamburger.innerHTML = "✖";
   }
 });
